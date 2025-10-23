feddbc7ef71a5015410f5979bf0f862bbc18759f
Richard Christian Wilkerson <richard.wilkerson@techconsulting.tech>
2025-10-23 16:09:59 -0400

    added detekt pluggins and subproject build to root build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,16 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+
+    // detekt plugin
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+}
+
+subprojects {
+    plugins.apply("io.gitlab.arturbosch.detekt")
+    extensions.configure(io.gitlab.arturbosch.detekt.extensions.DetektExtension::class) {
+        config.setFrom(files("$rootDir/config/detekt/config.yaml"))
+        buildUponDefaultConfig = true
+        parallel = true
+    }
 }
