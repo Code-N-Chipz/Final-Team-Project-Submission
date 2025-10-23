66e13ba36bce647532668edc980c2fbe0d326d06
Richard Christian Wilkerson <richard.wilkerson@techconsulting.tech>
2025-10-23 18:12:15 -0400

    made Colors explicitly Long

--- core/design/src/main/java/com/tc/design/theme/Color.kt
+++ core/design/src/main/java/com/tc/design/theme/Color.kt
@@ -3,23 +3,23 @@ package theme
 import androidx.compose.ui.graphics.Color
 
 // Primary brand colors
-val PrimaryColor = Color(0xFFFF791B)       // #ff791b
-val SecondaryColor = Color(0xFF14C972)     // #14c972
+val PrimaryColor = Color(0xFFFF791BL)       // #ff791b
+val SecondaryColor = Color(0xFF14C972L)     // #14c972
 
 // Icon & slider
-val PrimaryIconColor = Color(0xFFFFFFFF)   // #ffffff
-val SliderPrimary = Color(0xFF0DC970)      // #0dc970
+val PrimaryIconColor = Color(0xFFFFFFFFL)   // #ffffff
+val SliderPrimary = Color(0xFF0DC970L)      // #0dc970
 
 // Background
-val BackgroundColor = Color(0xFFF9FAFB)    // #f9fafb
+val BackgroundColor = Color(0xFFF9FAFBL)    // #f9fafb
 
 // Buttons
-val ButtonPrimary = Color(0xFFFF791B)      // Primary Progressive Button Color
-val ButtonSecondary = Color(0xFF0DC970)    // Second Progressive Button Color
-val ButtonTertiary = Color(0xFF027DFF)     // Tertiary button color
+val ButtonPrimary = Color(0xFFFF791BL)      // Primary Progressive Button Color
+val ButtonSecondary = Color(0xFF0DC970L)    // Second Progressive Button Color
+val ButtonTertiary = Color(0xFF027DFFL)     // Tertiary button color
 
 // Text colors
-val TextPrimary = Color(0xFFFFFFFF)        // Primary text color (white)
-val TextSecondary = Color(0xFFFF791B)      // Secondary text color (orange)
-val TextTertiary = Color(0xFFC4C8CF)       // Tertiary text color (grey)
-val TextQuaternary = Color(0xFF000000)     // Quaternary text (black)
+val TextPrimary = Color(0xFFFFFFFFL)        // Primary text color (white)
+val TextSecondary = Color(0xFFFF791BL)      // Secondary text color (orange)
+val TextTertiary = Color(0xFFC4C8CFL)       // Tertiary text color (grey)
+val TextQuaternary = Color(0xFF000000L)     // Quaternary text (black)
