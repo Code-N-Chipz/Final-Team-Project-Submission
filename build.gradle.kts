6de967d6918531e686887b16e642cb42ed20aa7c
Richard Christian Wilkerson <richard.wilkerson@techconsulting.tech>
2025-10-23 19:26:57 -0400

    h

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,13 @@ plugins {
 
 subprojects {
     plugins.apply("io.gitlab.arturbosch.detekt")
+    dependencies {
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
+    }
     extensions.configure(io.gitlab.arturbosch.detekt.extensions.DetektExtension::class) {
         config.setFrom(files("$rootDir/config/detekt/config.yaml"))
         buildUponDefaultConfig = true
         parallel = true
+        autoCorrect = true
     }
 }
