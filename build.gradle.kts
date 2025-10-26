f2886e58b73b568e8e56ff62ba11695c8f7320ca
tysonsunny <tyson.sunny@informationtechconsultants.co.uk>
2025-10-26 14:29:33 +0000

    changes for gradle with required dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,9 @@ plugins {
 
     // detekt plugin
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.5" apply false
 }
 
 subprojects {
--- mechanic/build.gradle.kts
+++ mechanic/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -34,10 +38,43 @@ android {
     }
 }
 
+//required for preview
+//composeCompiler {
+//    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+//    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf")
+//}
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+//  Added for compose support
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.foundation)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.runtime.livedata)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    implementation(libs.hilt.android)
+    ksp("com.google.dagger:hilt-android-compiler:2.57.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    implementation(libs.kotlinx.coroutines.play.services)
+    debugImplementation(libs.androidx.ui.tooling)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
