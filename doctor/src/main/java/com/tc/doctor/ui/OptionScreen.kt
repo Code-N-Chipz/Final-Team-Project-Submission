3f3648bdf14105083a6e69d2bf65ba168b5c98f3
Richard Christian Wilkerson <richard.wilkerson@techconsulting.tech>
2025-10-26 12:38:23 -0400

    switching branches

--- doctor/src/main/java/com/tc/doctor/ui/OptionScreen.kt
+++ doctor/src/main/java/com/tc/doctor/ui/OptionScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -15,8 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tc.design.R as CoreDraw
 import com.tc.doctor.R
 
@@ -30,7 +35,7 @@ fun OptionScreen() {
             horizontalArrangement = Arrangement.Start,
             modifier = Modifier.fillMaxWidth()
             ) {
-            IconButton(onClick = {}) {
+            IconButton(onClick = {  } ) {
                 Icon(
                     painter = painterResource(CoreDraw.drawable.arrow_left_orange_icon),
                     contentDescription = "Back Button"
@@ -42,12 +47,21 @@ fun OptionScreen() {
             contentDescription = "Filler Image",
             modifier = Modifier.size(300.dp)
         )
+        Spacer(modifier = Modifier.padding(vertical = 10.dp))
         Text(
-            text = "Choose Path"
+            text = "Choose your option",
+            style = theme.typography.titleLarge.copy(fontWeight = FontWeight.Bold)
         )
+        Spacer(modifier = Modifier.padding(vertical = 10.dp))
         Text(
-            text = "We're here to help"
+            text = "We're here to help",
+            style = theme.typography.bodyMedium,
+            // TODO: need to fix color to be Pent
+            color = theme.textQuaternary,
+            textAlign = TextAlign.Center
         )
+        Spacer(modifier = Modifier.padding(vertical = 30.dp))
+
         Button(onClick = {}) {
             Text("Make the Diagnosis")
         }
