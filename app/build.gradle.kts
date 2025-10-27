33efffee3a64e5c9c0a4f7abf5bababf962f13d3
Ali Youssef <ali.youssef@informationtechconsultants.co.uk>
2025-10-27 15:40:16 +0000

    extra

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
     implementation(project(":laundry"))
+    implementation(project(":delivery"))
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
